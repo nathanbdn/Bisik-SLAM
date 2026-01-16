e707625755c9962737fea5bb9c70d8801278c542
Nathan Bodin <nathan.bodin17@gmail.com>
2026-01-16 11:29:24 +0100

    ajout du volume, changement de la partie modification

--- actions/updateChoregraphie.php
+++ actions/updateChoregraphie.php
@@ -5,14 +5,16 @@ $angle = filter_input(INPUT_POST, 'angle', FILTER_VALIDATE_INT);
 $duree = filter_input(INPUT_POST, 'duree', FILTER_VALIDATE_INT);
 $musique = filter_input(INPUT_POST, 'musique', FILTER_DEFAULT);
 $id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
+$volume = filter_input(INPUT_POST,'volume',FILTER_DEFAULT);
 include "../config.php";
 $pdo = new PDO("mysql:host=" . config::HOST . ";dbname=" . config::DBNAME, config::USER, config::PASSWORD);
-$req = $pdo->prepare("update choregraphie set message =:message, mouvement_angle = :angle, mouvement_duree = :duree, musique = :musique   where id=:id");
+$req = $pdo->prepare("update choregraphie set message =:message, mouvement_angle = :angle, mouvement_duree = :duree, musique = :musique, volume = :volume   where id=:id");
 $req->bindParam(':message', $message);
 $req->bindParam(':angle', $angle);
 $req->bindParam(':duree', $duree);
 $req->bindParam(':musique', $musique);
 $req->bindParam(':id', $id);
+$req->bindParam(':volume', $volume);
 $req->execute();
 
 header("Location: ../index.php");
--- modifierChoregraphie.php
+++ modifierChoregraphie.php
@@ -1,6 +1,9 @@
 <?php
 
-
+include "header.php";
+$dir = "son/";
+$files = scandir($dir);
+$extensions = ['mp3','wav','ogg'];
 
 // on récupère l'id
 $id = filter_input(INPUT_GET,"id",FILTER_VALIDATE_INT);
@@ -23,16 +26,27 @@ $choregraphies = $choregraphies[0]
 <form action="actions/updateChoregraphie.php" method="post">
     <input type="hidden" name="id" value="<?php echo $id ?>">
 
-    Message: <input value = "<?php echo htmlentities($choregraphies["message"])?>" type="text" required maxlength="50" name="message">
-    <br>
+    Message: <br><input value = "<?php echo htmlentities($choregraphies["message"])?>" type="text" required maxlength="50" name="message">
+    <br><br>
     mouvement(angle) : <input value = "<?php echo htmlentities($choregraphies["mouvement_angle"])?>" type="number" required name="angle">
-    <br>
+    <br><br>
     Description(durée) : <input value = "<?php echo htmlentities($choregraphies["mouvement_duree"])?>" type="number" required name="duree">
-    <br>
-    Musique : <br>
-    <textarea name="musique">"<?php echo htmlentities($choregraphies["musique"])?>"</textarea>
-
-
+    <br><br>
+    volume : <input value = "<?php echo htmlentities($choregraphies["volume"])?>" type="number" required name="volume">
+    <br><br>
+    Musique :<br>
+    <select name="musique" required>
+        <option value="">-- Choisir une musique --</option>
+        <?php
+        foreach ($files as $file) {
+            if (in_array(pathinfo($file, PATHINFO_EXTENSION), $extensions)) {
+                $selected = ($file == $choregraphies['musique']) ? "selected" : "";
+                echo "<option value=\"$file\" $selected>$file</option>";
+            }
+        }
+        ?>
+    </select>
+    <br><br>
     <input type="submit" value="OK">
 
 </form>
