37c6537e7c5117507ab9d9b9303a72aeb9e588c6
Nathan Bodin <nathan.bodin17@gmail.com>
2026-01-16 11:27:03 +0100

    ajout de la partie volume sur la BDD, changement de l'ajout.

--- actions/insertChoregraphie.php
+++ actions/insertChoregraphie.php
@@ -1,22 +1,23 @@
 <?php
 
-
 //on récupère les données du POST
 $message = filter_input(INPUT_POST, 'message', FILTER_DEFAULT);
 $angle = filter_input(INPUT_POST, 'angle', FILTER_VALIDATE_INT);
 $duree = filter_input(INPUT_POST, 'duree', FILTER_VALIDATE_INT);
 $musique = filter_input(INPUT_POST, 'musique', FILTER_DEFAULT);
+$volume = filter_input(INPUT_POST,'volume',FILTER_DEFAULT);
 
 include "../config.php";
 $pdo = new PDO('mysql:host=' . config::HOST . ';dbname=' . config::DBNAME
     , config::USER, config::PASSWORD);
 
 //on prépare la requête avec des bindParam pour éviter les injections SQL
-$req=$pdo->prepare("INSERT INTO choregraphie (message, mouvement_angle, mouvement_duree, musique) VALUES (:message, :angle, :duree, :musique)");
+$req=$pdo->prepare("INSERT INTO choregraphie (message, mouvement_angle, mouvement_duree, musique, volume) VALUES (:message, :angle, :duree, :musique, :volume)");
 $req->bindParam(':message', $message);
 $req->bindParam(':angle', $angle);
 $req->bindParam(':duree', $duree);
 $req->bindParam(':musique', $musique);
+$req->bindParam(':volume', $volume);
 
 $req->execute();
 
--- ajouterChoregraphie.php
+++ ajouterChoregraphie.php
@@ -1,14 +1,46 @@
+<?php
+include "header.php";
+$volume = 40;
+$dir = "son/";
+$files = scandir($dir);
+?>
+
+
+
 
 <h1>Ajouter une chorégraphie</h1>
 
 <form action="actions/insertChoregraphie.php" method="post">
-    Message: <input type="text" required maxlength="50" name="message">
+    Message: <br><input type="text" required maxlength="50" name="message">
+    <br><br>
+    mouvement(angle) : <br><input type="int" required name="angle">
+    <br><br>
+    mouvement(durée) : <br><input type="int" required name="duree">
+    <br><br>
+    Musique : <br><select name="musique" id="musique" required>
+        <option value="">-- Choisir une musique --</option>
+
+        <?php
+        foreach ($files as $file) {
+            if ($file !== "." && $file !== "..") {
+                echo "<option value=\"$file\">$file</option>";
+            }
+        }
+        ?>
+    </select>
+    <br><br>
+    volume : <span id="val"><?= $volume ?></span>%
     <br>
-    mouvement(angle) : <input type="int" required name="angle">
+    <input
+            type="range"
+            min="0"
+            max="100"
+            value="<?= $volume ?>"
+            name="volume"
+            oninput="document.getElementById('val').textContent = this.value"
+    >
     <br>
-    Description(durée) : <input type="int" required name="duree">
-    <br>
-    Musique : <input type="text" required maxlength="500" name="musique">
+
     <input type="submit" value="OK">
 
 </form>
